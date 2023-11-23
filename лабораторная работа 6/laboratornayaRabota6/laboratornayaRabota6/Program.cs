4fcc25f31000ca283b52aa102adab6df10aa0d0b
aleksandrbaranov1 <144918786+aleksandrbaranov1@users.noreply.github.com>
2023-11-23 20:32:19 +0200

    Add files via upload

---
+++ лабораторная работа 6/laboratornayaRabota6/laboratornayaRabota6/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace laboratornayaRabota6;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double b0, bx, db, a, eps = 0.001, b, l;
+        bool flg1 = false, flg2 = false, flg3 = false, flg4 = false;
+        int count;
+        Console.Write("Введите начальный диапазон B: ");
+        flg1 = double.TryParse(Console.ReadLine().Replace(',', '.'), out b0);
+        Console.Write("Введите конечный диапазон B: ");
+        flg2 = double.TryParse(Console.ReadLine().Replace(',', '.'), out bx);
+        Console.Write("Введите шаг для диапазона от B0 до до BX: ");
+        flg3 = double.TryParse(Console.ReadLine().Replace(',', '.'), out db);
+        Console.Write("Введите значения для переменной a: ");
+        flg4 = double.TryParse(Console.ReadLine().Replace(',', '.'), out a);
+        if (flg1 && flg2 && flg3 && flg4)
+        {
+            count = 0;
+            while (b0 <= bx)
+            {
+                b = b0;
+                count += 1;
+                flg1 = Math.Abs(Math.Cos(b)) >= eps;
+                flg2 = Math.Abs(Math.Pow((Math.Pow(Math.E, a) - (b * b * b) + Math.Tan(b)), 1f / 3f)) >= eps;
+                if (flg1 && flg2)
+                {
+                    l = (24.2 * Math.Cos(a * a) + Math.Sin(a + b)) / (Math.Pow((Math.Pow(Math.E, a) - (b * b * b) + Math.Tan(b)), 1f / 3f));
+                    Console.WriteLine($"{count})При B = {b0} L(B) = {l}");
+                }
+                else
+                {
+                    Console.WriteLine($"{count})При B = {b0}, L(B) не сущетвует");
+                }
+
+                b0 += db;
+                if (count % 20 == 0)
+                {
+                    Console.ReadKey(true);
+                }
+            }
+        }
+        else
+        {
+            Console.WriteLine("Введены неверные данные");
+        }
+
+        Console.ReadKey(true);
+
+    }
+}
+
